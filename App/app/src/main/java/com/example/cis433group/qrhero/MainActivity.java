a660a2bd8649df780580707d3052b6195a5a8da3
Erik Lopez Raya <eriklopezraya@gmail.com>
2018-02-05 11:47:31 -0800

    init

---
+++ App/app/src/main/java/com/example/cis433group/qrhero/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.cis433group.qrhero;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, 10);
+        }
+        final SurfaceView cameraView = (SurfaceView)findViewById(R.id.camera);
+        TextView barcodeInfo = (TextView)findViewById(R.id.link);
+        BarcodeDetector codeDetector = new BarcodeDetector.Builder(this).build();
+        final CameraSource cameraSource = new CameraSource.Builder(this, codeDetector).setRequestedFps(15.0f).build();
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try{
+                    cameraSource.start(cameraView.getHolder());
+                } catch (IOException ioe) {
+
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+    }
+}
